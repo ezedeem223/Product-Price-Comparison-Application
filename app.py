35a5bd1159e778a9b54cd3556d61badab25fdd58
Mohamad Sabbagh <169142368+ezedeem223@users.noreply.github.com>
2026-03-07 00:49:11 +0300

    Add personal header comments to app.py - Ezedeen (ezedeem223)

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# Product Price Comparison Microservice - Customized by Ezedeen (ezedeem223)
+# IBM Final Project: Application Development using Microservices and Serverless
+# Author: Ezedeen | GitHub: ezedeem223 | March 2026
+
 from flask import Flask
 from flask_cors import CORS
 from flask import send_from_directory
@@ -12,3 +16,6 @@ def getHomePage():
 if __name__=="__main__":
     app.run(port=5001,debug=True) 
     # When no port is specified, starts at default port 5000
+
+
+# Powered by Ezedeen (ezedeem223) - IBM Microservices Final Project 2026
